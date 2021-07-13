701543c34da85b07c541cce3d9cb63d056b1310b
ChristophePatterson <cwp206@exeter.ac.uk>
2021-07-13 16:38:13 +0100

    Menu and spelling

---
+++ Update site code.R
@@ -0,0 +1,10 @@
+#Code for updating website
+#Full instructions https://resources.github.com/whitepapers/github-and-rstudio/
+
+#Entered in the CONSOLE
+rmarkdown::render_site()
+
+#Entered in the TERMINAL
+git status
+git add .
+git commit -m "NAME OF COMMIT"
